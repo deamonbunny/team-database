e0c3b358ae129abd23f5a935fda1a7e8cdd5306c
bunny <deamonbunny@gmail.com>
2022-08-21 21:14:08 -0700

    updated seeds, schema and index to remove errors

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const env = require("dotenv").config();
 const express = require('express');
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
-const conTable = require(`console.table`)
+const conTable = require('console.table');
 
 
 
@@ -11,34 +11,21 @@ const exp = express();
 
 const db = mysql.createConnection(
     {
-      host: process.env.HOST,
-      user: process.env.USER,
-      password: process.env.PASSWORD,
-      database: process.env.DATABASE
+      host: 'localhost',
+      user: 'root',
+      password: 'password',
+      database: 'company_db'
     },
     console.log(`Connected to the company_db database.`)
   );
 
-
-  exp.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
-
 const registry = () => {
-  inquirer.createPromptModule([
+  inquirer.prompt([
     {
       Type: `list`,
       name: `selections`,
-      message: `Please select an option from the list below to continue`,
-      choices: [
-        "All Departments",
-        "All Team Roles",
-        "All Employees",
-        "Add Department",
-        "Add Team Role",
-        "Add Employee",
-        "Exit"
-      ]
+      message: "Please select an option from the list below to continue",
+      choices: ["All Departments", "All Team Roles", "All Employees", "Add Department", "Add Team Role", "Add Employee", "Update Employee", "Exit"],
     }
   ]).then((ans => {
       //Checks to see if all departments selected
@@ -72,8 +59,116 @@ const registry = () => {
     }else if (ans.selections === "Add Employee") {
       genEmployee()
       //Checks to see if Exit is selected
+    }else if (ans.selections === "Update Employee") {
+      updateEmployee()
+      //Checks to see if Exit is selected
     }else {
       console.log("Thank You, have a nice day!")
     }}
 ))};
 registry()
+
+const genDepart = () =>{
+  inquirer.prompt([
+    {
+      type: `input`,
+      name: `department`,
+      message: `New department name?`
+    }
+  ]).then((ans => {
+    db.query(`INSERT INTO department(name) VALUES (?)`, [ans.department], (err,result) => {
+      console.log(`${ans.department} has been added to the database.`)
+      registry()
+    })
+  }))
+};
+
+const genRole = () =>{
+  inquirer.prompt([
+    {
+      type: `input`,
+      name: `newRole`,
+      message: "New Role in Company?",
+  },
+  {
+      type: `number`,
+      name: `salary`,
+      message: "Role Salary?",
+  },
+  {
+      type: `list`,
+      name: `department`,
+      message: "Role belongs to what Department?",
+      choices: `SELECT department.name AS name, department.id AS value FROM department`,
+  },
+  ]).then((ans => {
+    db.query(`INSERT INTO role(title, salary, department_id) VALUES (?, ?, ?)`,
+    [
+      ans.newRole,
+      ans.salary,
+      ans.department,
+    ], (err,result) => {
+      console.log(`${ans.newRole} has been added to ${ans.department} in the database.`)
+      registry()
+    })
+  }))
+};
+
+const genEmployee = () =>{
+  inquirer.prompt([
+    {
+      type: `input`,
+      name: `first`,
+      message: "Employee's first name?",
+  },
+  {
+      type: `input`,
+      name: `last`,
+      message: "Employee's last name?",
+  },
+  {
+      type: `list`,
+      name: `role`,
+      message: "Employee Role?",
+      choices: `SELECT role.title AS name, role.id AS value FROM role`,
+  },
+  {
+      type: `input`,
+      name: `manager`,
+      message: "Manager ID?"
+  }
+  ]).then((ans => {
+    db.query(`INSERT INTO employee(first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)`,
+    [
+      ans.first,
+      ans.last,
+      ans.role,
+      ans.manager
+    ], (err,result) => {
+      console.log(`${ans.first} ${ans.last} has been added to the database.`)
+      registry()
+    })
+  }))
+};
+
+const updateEmployee = () => {
+  inquirer.prompt([
+    {
+      type: `list`,
+      name: `employee`,
+      message: "Which Employee to Update?",
+      choices: `SELECT employee.first_name AS name, employee.id AS value FROM employee`
+  },
+  {
+      type: `list`,
+      name: `role`,
+      message: "New Role?",
+      choices: `SELECT role.title AS name, role.id AS value FROM role`
+  },
+  ]).then((ans => {
+    db.query(`UPDATE employee SET role_id = ${ans.role} WHERE name = ${ans.employee}`), (err,result) => {
+      console.log(`${ans.first} ${ans.last} has been added to the database.`)
+      registry()
+    }
+  }))
+}
